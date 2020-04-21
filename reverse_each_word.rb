b4c0c3e1f000c6d7853ca98f34c9f02909ffc494
Daniel Vo <daniiielvo@gmail.com>
2020-04-21 19:56:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,15 @@ end
 newarray.join(" ")
 end
 
+def reverse_each_word(words)
+  newarray = []
+  array=words.split (" ")
+  array.each do |word|
+    newarray << word.reverse
+end
+newarray.join(" ")
+end
+
 
 
 
